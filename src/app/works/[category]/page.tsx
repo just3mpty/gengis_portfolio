d298e4dba9a1d1e3802dffa000a1b1aa1a88771d
3mpty <aurelien.corre@outlook.fr>
2024-08-20 18:16:33 +0200

    Created projects page (style done too)

--- src/app/works/[category]/page.tsx
+++ src/app/works/[category]/page.tsx
@@ -1,6 +1,9 @@
 "use client";
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
+import styles from "@/styles/works.module.css";
+import Footer from "@/components/footer/Footer";
+import Link from "next/link";
 
 interface DataType {
     category: string;
@@ -9,48 +12,80 @@ interface DataType {
             title: string;
             images: string[];
             date: string;
+            description: string;
+            tools: string[];
         }
     ];
 }
 
 const Category = ({ params }: { params: { category: string } }) => {
     const { category } = params;
+    const [otherCategory, setOtherCategory] = useState<string>("");
     const [data, setData] = useState<DataType[]>([]);
 
     useEffect(() => {
         const fetchProjects = async () => {
             const projects = await fetch("/projects.json");
             const data = await projects.json();
-            setData(data);
+            const filteredData = data.filter(
+                (cat: DataType) => cat.category === category
+            );
+            setData(filteredData);
         };
+
+        if (category === "freelance") {
+            setOtherCategory("personnal");
+        } else {
+            setOtherCategory("freelance");
+        }
+
         fetchProjects();
-    }, []);
+    }, [category]);
 
     return (
-        <section>
-            <h1>{category} projects</h1>
-            <div>
-                {data
-                    .filter((cat) => cat.category === category)
-                    .map((projects) =>
-                        projects.projects.map((project, idx) => (
-                            <div key={idx}>
-                                <h1>{project.title}</h1>
+        <section className={styles.main}>
+            <h1>
+                {category} <br /> <span>projects</span>
+            </h1>
+            {data.map((projects) =>
+                projects.projects.map((project, idx) => (
+                    <div className={styles.project} key={idx}>
+                        <div className={styles.text}>
+                            <div className={styles.title}>
+                                <h2>{project.title}</h2>
                                 <p>{project.date}</p>
-                                {project.images.map((image, idx) => (
-                                    <figure key={idx}>
-                                        <Image
-                                            src={image}
-                                            alt={`Project image n°${idx + 1}`}
-                                            width={50}
-                                            height={50}
-                                        />
-                                    </figure>
-                                ))}
                             </div>
-                        ))
-                    )}
-            </div>
+                            <div className={styles.details}>
+                                <p>
+                                    <span>tools :</span>{" "}
+                                    {project.tools.map((tool) => tool + " / ")}
+                                </p>
+                                <p>
+                                    <span>description :</span>{" "}
+                                    {project.description}
+                                </p>
+                            </div>
+                        </div>
+                        <div className={styles.images}>
+                            {project.images.map((image, idx) => (
+                                <figure key={idx}>
+                                    <Image
+                                        src={image}
+                                        alt={`Project image n°${idx + 1}`}
+                                        fill
+                                    />
+                                </figure>
+                            ))}
+                        </div>
+                    </div>
+                ))
+            )}
+
+            <Link className={styles.button} href={`/works/${otherCategory}`}>
+                check my {otherCategory} projects
+            </Link>
+
+            <Footer />
         </section>
     );
 };
